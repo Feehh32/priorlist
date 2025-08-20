3647e0359b852970684577e465a191540dbc0a0b
Fernando Pereira <fernando.mkv@gmail.com>
2025-08-20 14:41:09 -0300

    finishing register-page-layout

---
+++ src/components/input/FormInput.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const FormInput = ({ label, placeholder, type = "text", id }) => {
+  return (
+    <label htmlFor={id} className="text-text-main font-semibold font-secondary">
+      {label}
+      <input
+        type={type}
+        name={id}
+        id={id}
+        placeholder={placeholder}
+        className="block border border-input-border rounded-lg w-full px-4 py-3 font-normal focus:border-primary outline-none focus:border-2 focus:ring-2 focus:ring-primary/40 font-primary shadow-sm bg-bg-main"
+      />
+      <span className="text-red-500 text-sm font-primary hidden">
+        o erro vai aqui
+      </span>
+    </label>
+  );
+};
+
+export default FormInput;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,58 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import FormInput from "../components/input/FormInput";
 
 const Register = () => {
   return (
-    <div>
-      <h1 className="text-2xl font-bold">Registrar no PriorList</h1>
-    </div>
+    <main className="min-h-screen flex items-center justify-center bg-gray-50 px-4">
+      <section className=" bg-white max-w-2xl w-full shadow-md rounded-2xl">
+        <Link
+          to="/"
+          className="text-lg text-primary font-semibold text-center pt-4 pb-2 font-logo block"
+          aria-label="Priolist - Página inicial"
+        >
+          Priorlist
+        </Link>
+        <h2 className=" text-xl md:text-2xl font-medium text-center text-secondary mb-8">
+          Crie aqui sua conta gratuitamente
+        </h2>
+        <form className="px-8 pb-8 grid gap-4">
+          <FormInput label="Nome" placeholder="Seu nome" id="name" />
+          <FormInput
+            label="Email"
+            placeholder="Seu email"
+            id="email"
+            type="email"
+          />
+          <FormInput
+            label="Senha"
+            placeholder="Sua senha"
+            id="password"
+            type="password"
+          />
+          <FormInput
+            label="Confirme sua senha"
+            placeholder="Confirme sua senha"
+            id="confirm-password"
+            type="password"
+          />
+          <button
+            type="submit"
+            className="w-full bg-primary text-white font-semibold py-3 rounded-lg hover:bg-blue-700 transition-colors mt-2 cursor-pointer font-secondary shadow-md"
+          >
+            Registrar
+          </button>
+          <p className="text-sm text-center text-gray-600">
+            Já tem conta?{" "}
+            <Link
+              to="/login"
+              className="text-primary font-medium hover:underline"
+            >
+              Entre aqui
+            </Link>
+          </p>
+        </form>
+      </section>
+    </main>
   );
 };
 
